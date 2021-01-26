82ba3bfd566d2592a0642238b11dd08da2dec236
UruYoba <sotnikov97@mail.ru>
2021-01-26 22:47:47 +0300

    add brain-even

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,28 @@
 import readline from 'readline-sync';
 
-export default () => {
+export const greeting = () => {
   const name = readline.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
+};
+export const even = () => {
+  const name = greeting();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
+  let counter = 0;
+  while (counter < 3) {
+    const number = Math.floor(Math.random() * (1001 - 1)) + 1;
+    const correctAnswer = isEven(number);
+    console.log(`Question: ${number}`);
+    const answer = readline.question('Your answer: ');
+    if (answer === correctAnswer) {
+      counter += 1;
+      console.log('Correct!');
+    } else {
+      counter = 0;
+      console.log(`'${answer} is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+    }
+  }
+  console.log(`Congratulations, ${name}`);
 };
